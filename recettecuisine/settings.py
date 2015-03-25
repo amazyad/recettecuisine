70fb73d918f8b7d9b55d5a299c776a1c457b1845
Ahmad Mazyad <ahmad.mazyad@hotmail.com>
2015-03-25 23:10:55 +0100

    Add Environments configuration

--- recettecuisine/settings.py
+++ recettecuisine/settings.py
@@ -8,91 +8,165 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from configurations import Configuration
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DATABASE_DIR = os.path.join(BASE_DIR, 'databases')
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+class Base(Configuration):
+    """
+    Basic configuration
+    """
+    SITE_ID = 1
+    
+    DEBUG = True
+    TEMPLATE_DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b%s4w-g6fnj#8lqcrg%r%w5-d#m^f3^91r7ir$8&k^z2_6d55y'
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'b%s4w-g6fnj#8lqcrg%r%w5-d#m^f3^91r7ir$8&k^z2_6d55y'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    ALLOWED_HOSTS = []
 
-TEMPLATE_DEBUG = True
+    # Application definition
 
-ALLOWED_HOSTS = []
+    INSTALLED_APPS = (
+        # admin tools
+        'admin_tools',
+        'admin_tools.theming',
+        'admin_tools.menu',
+        'admin_tools.dashboard',
+        # default apps
+        'django.contrib.sites',
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        # other apps
+        'rest_framework',
+        'jquery',
+        'sitecuisine',
+    )
 
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
 
-# Application definition
+    TEMPLATE_CONTEXT_PROCESSORS = (
+        'django.contrib.auth.context_processors.auth',
+        'django.core.context_processors.debug',
+        'django.core.context_processors.i18n',
+        'django.core.context_processors.media',
+        'django.core.context_processors.static',
+        'django.core.context_processors.tz',
+        'django.contrib.messages.context_processors.messages',
+        'django.core.context_processors.request',
+    )
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'debug_toolbar',
-    'rest_framework',
-    'jquery',
-    'sitecuisine',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-)
-
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
-
-ROOT_URLCONF = 'recettecuisine.urls'
-
-WSGI_APPLICATION = 'recettecuisine.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    REST_FRAMEWORK = {
+        # Use Django's standard `django.contrib.auth` permissions,
+        # or allow read-only access for unauthenticated users.
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        ]
     }
-}
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+    ROOT_URLCONF = 'recettecuisine.urls'
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+    WSGI_APPLICATION = 'recettecuisine.wsgi.application'
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+    # Database
+    # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-STATIC_URL = '/static/'
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(DATABASE_DIR, 'db.sqlite3'),
+        },
+    }
+
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'UTC'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+    STATIC_URL = '/static/'
+
+
+class Prod(Base):
+    # Debug configuration
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+    INTERNAL_IPS = ['*', ]
+    ALLOWED_HOSTS = INTERNAL_IPS
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(DATABASE_DIR, 'db_prod.sqlite3'),
+        },
+    }
+
+
+class Dev(Base):
+    # Debug configuration
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+    INTERNAL_IPS = ['localhost', '127.0.0.1', '::1', ]
+    ALLOWED_HOSTS = INTERNAL_IPS
+    # Django Debug Toolbar
+    INSTALLED_APPS = Base.INSTALLED_APPS + ('debug_toolbar', )
+    MIDDLEWARE_CLASSES = ('debug_toolbar.middleware.DebugToolbarMiddleware', ) + Base.MIDDLEWARE_CLASSES
+    DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(DATABASE_DIR, 'db_dev.sqlite3'),
+        },
+    }
+
+
+class Test(Base):
+    # Debug configuration
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+    INTERNAL_IPS = ['localhost', '127.0.0.1', '::1', ]
+    ALLOWED_HOSTS = INTERNAL_IPS
+    # Django Debug Toolbar
+    INSTALLED_APPS = Base.INSTALLED_APPS + ('debug_toolbar', )
+    MIDDLEWARE_CLASSES = Base.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+    DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(DATABASE_DIR, 'db_test.sqlite3'),
+        },
+    }
--- recettecuisine/urls.py
+++ recettecuisine/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('sitecuisine.urls', namespace='sitecuisine')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin_tools/', include('admin_tools.urls')),
 )
 
 if settings.DEBUG:
